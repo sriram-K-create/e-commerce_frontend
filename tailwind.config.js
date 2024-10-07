5662c0556c15d4e0fe48657879add1032c66c242
SRIRAM K <67730995+sriram-K-create@users.noreply.github.com>
2024-10-07 08:28:40 +0530

    Adding a Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+   "./src/customer/components/Navigation/Navigation.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
